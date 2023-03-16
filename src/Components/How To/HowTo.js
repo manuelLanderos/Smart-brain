fb372174a6e16d1f0c3be62bbae015fb8e3105e3
manuelLanderos <108149974+manuelLanderos@users.noreply.github.com>
2023-03-16 16:04:57 -0400

    little edit

--- src/Components/How To/HowTo.js
+++ src/Components/How To/HowTo.js
@@ -11,7 +11,7 @@ const HowTo = ({ onRouteChange }) => {
         <a href="https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg?cs=srgb&dl=pexels-mateus-souza-3586798.jpg&fm=jpg">Copy This Link</a>
         <p class="ttc">and paste it into the detect bar and watch as the smart brain recognizes a face in the image!</p>
       </div>
-      <button onClick={() => onRouteChange('signin')} className="f3 link dim black underline pa3 pointer">Go Back</button>
+      {/* <button onClick={() => onRouteChange('signin')} className="f3 link dim black underline pa3 pointer">Go Back</button> */}
       <div >
 
       </div>
