75b86cf566698b164dcb7ba992be1f221aa0f090
manuelLanderos <108149974+manuelLanderos@users.noreply.github.com>
2023-03-21 13:54:12 -0400

    small tiny changes

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -5,7 +5,7 @@ const Rank = ({ name, entries }) => {
   return (
     <div>
       <div className="white f3">
-        {`${name}, your current entries are...`}
+        {`${name}, Your Current Entries Are...`}
       </div>
       <div className="white f1">
         {entries}
